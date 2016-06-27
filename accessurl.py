964567490fb08f7a8c16871f2c092d1722d8ab94
fendoumo <fendou_mo@163.com>
2016-06-28 00:12:10 +0800

    update py ,每分钟访问一次网页

--- accessurl.py
+++ accessurl.py
@@ -9,10 +9,11 @@ def getHttp(url):
     http_hd = urllib.urlopen(url)
     return http_hd.read()
 
-
-http_txt = getHttp('http://abcabc.gq')
-print http_txt
-http_txt = getHttp('http://window.gq/')
-print http_txt
-http_txt = getHttp('http://mytool.gq/')
-print http_txt
+while True:
+    http_txt = getHttp('http://abcabc.gq')
+    print http_txt
+    http_txt = getHttp('http://window.gq/')
+    print http_txt
+    http_txt = getHttp('http://mytool.gq/')
+    print http_txt
+    time.sleep(60)
